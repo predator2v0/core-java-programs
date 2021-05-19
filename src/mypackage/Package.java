44766c6364b78073d8afb16f068f0998e1eb46da
subhakanta roul <papu.roul99@gmail.com>
2021-05-19 14:59:10 +0530

    demonstrating creation of package and their use in java

---
+++ src/mypackage/Package.java
@@ -0,0 +1,19 @@
+package mypackage;
+
+public class Package {
+    public static void main(String[] args) {
+
+    }
+
+    public static void cheers() {
+        System.out.println("static method importing");
+    }
+
+    public void welcome() {
+        System.out.println("welcome to my custom package");
+    }
+
+    public void bye() {
+        System.out.println("bye from custompackage!!");
+    }
+}
---
+++ src/mypackage/subpack/subPackage.java
@@ -0,0 +1,13 @@
+package mypackage.subpack;
+
+public class subPackage {
+    public static void main(String[] args) {
+        
+    }
+    public void subwelcome(){
+        System.out.println("welcome from subpackage!");
+    }
+    public void subbye(){
+        System.out.println("bye!! from subpackage!");
+    }
+}
