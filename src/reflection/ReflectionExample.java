d03cf960e92df4257ab102352beaf75d128e9bd1
subhakanta roul <papu.roul99@gmail.com>
2021-05-20 23:05:31 +0530

    reflection programs made more readable

--- src/reflection/ReflectionExample.java
+++ src/reflection/ReflectionExample.java
@@ -7,13 +7,13 @@ public class ReflectionExample {
 	public static void main(String[] args) throws ClassNotFoundException {
 		Class c =  Class.forName("reflection.ReflectionStudent");
 		Class i = Class.forName("reflection.ReflectionInterface");
-		System.out.println(c.getName());
-		System.out.println(i.getName());
+		System.out.println("c.getName(): "+c.getName());
+		System.out.println("i.getName(): "+i.getName());
 		
-		System.out.println(i.isInterface());
+		System.out.println("i.isInterface(): "+i.isInterface());
 		int[] ar = new int[5];
-		System.out.println(ar.getClass().isArray());
-		System.out.println(ar.getClass().isPrimitive());
+		System.out.println("ar.getClass().isArray(): "+ar.getClass().isArray());
+		System.out.println("ar.getClass().isPrimitive(): "+ar.getClass().isPrimitive());
 		
 	}
 
