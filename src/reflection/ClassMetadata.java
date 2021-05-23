2223fa85de3c70503396cd388d59717ba8824c79
subhakanta roul <papu.roul99@gmail.com>
2021-05-23 22:20:28 +0530

    retriving metadata of class during runtime

---
+++ src/reflection/ClassMetadata.java
@@ -0,0 +1,14 @@
+package reflection;
+
+public class ClassMetadata {
+
+	public static void main(String[] args) throws ClassNotFoundException {
+		// TODO Auto-generated method stub
+		Class classes = Class.forName("reflection.ReflectionStudent");
+		System.out.println("name: "+classes.getName());
+		System.out.println("type name: "+classes.getTypeName());
+		System.out.println("package name: "+classes.getPackage());
+		System.out.println("number of modifiers: "+classes.getModifiers());
+	}
+
+}
