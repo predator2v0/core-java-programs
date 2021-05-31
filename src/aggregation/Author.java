074652d77e5d4c57fabd1cd016d3f28b3d8e9032
subhakanta roul <papu.roul99@gmail.com>
2021-05-31 22:24:26 +0530

    source file internal documentation added

--- src/aggregation/Author.java
+++ src/aggregation/Author.java
@@ -1,10 +1,11 @@
 package aggregation;
 
-class Author{
-    String name;
-    int age;
-    Author(String nm, int ag){
-        name = nm;
-        age = ag;
-    }
+class Author {
+	String name;
+	int age;
+
+	Author(String nm, int ag) {
+		name = nm;
+		age = ag;
+	}
 }
