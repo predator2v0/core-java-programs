9e5b8d45a0326c02d0ba2fc5a254de4dbe031535
subhakanta roul <papu.roul99@gmail.com>
2021-05-31 22:25:28 +0530

    source file internal documentation added

--- src/aggregation/Book.java
+++ src/aggregation/Book.java
@@ -1,16 +1,21 @@
 package aggregation;
 
-class Book{
-    String name;
-    int id;
-    Author auth;
-    Book(String nm, int id, Author at){
-        name = nm;
-        this.id = id;
-        auth = at;
-    }  
-    void viewDetails(){
-        System.out.println("book id: "+id+"\t book name: "+name);
-        System.out.println("author name: "+auth.name+"\t age: "+auth.age);
-    }
+class Book {
+	String name;
+	int id;
+//  object of author class is created in book class directly without inheritance.
+//  creating a has-A relationship and loosely coupled classes.
+//  hence making both classes in the aggregation relationship.
+	Author auth;
+
+	Book(String nm, int id, Author at) {
+		name = nm;
+		this.id = id;
+		auth = at;
+	}
+
+	void viewDetails() {
+		System.out.println("book id: " + id + "\t book name: " + name);
+		System.out.println("author name: " + auth.name + "\t age: " + auth.age);
+	}
 }
