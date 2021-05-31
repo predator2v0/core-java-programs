41c579564efcb22d289c6705dfbf0e60813c86c9
subhakanta roul <papu.roul99@gmail.com>
2021-05-31 22:21:48 +0530

    source file internal documentation added

--- src/abstractClass/abclass.java
+++ src/abstractClass/abclass.java
@@ -4,30 +4,33 @@ package abstractClass;
 // abstract class is a class declared with the abstract keyword.
 // it can hold both abstract as well as non-abstract(concrete) methods.
 abstract class abclass extends baclass {
-    int m = 5, n;
-    
+	int m = 5, n;
+
 // constructor of abstract class
 // abstract class can have constructor but cannot be instantiated i.e. it cannot create objects of its own.
-    abclass() {
-        System.out.println("constructor from abstract class invoked");
-    }
-//  abstract method in abstract class.
-    public abstract void abm();
-//  concrete method in abstract class.
-    public void show() {
-        System.out.println("non abstract method inside abstract class");
-    }
+	abclass() {
+		System.out.println("constructor from abstract class invoked");
+	}
 
-    void sum() {
-        System.out.println("enter both the numbers:");
-        java.util.Scanner sc = new java.util.Scanner(System.in);
-        m = sc.nextInt();
-        n = sc.nextInt();
-        sc.close();
-        System.out.println("sum: " + (m + n));
-    }
+//  abstract method in abstract class.
+	public abstract void abm();
+
+//  concrete method in abstract class.
+	public void show() {
+		System.out.println("non abstract method inside abstract class");
+	}
+
+	void sum() {
+		System.out.println("enter both the numbers:");
+		java.util.Scanner sc = new java.util.Scanner(System.in);
+		m = sc.nextInt();
+		n = sc.nextInt();
+		sc.close();
+		System.out.println("sum: " + (m + n));
+	}
+
 //  overrides abstract method from parent class but doesn't define its body.
 //  its body can be defined in the concrete class inherits this class.
-    @Override
-    abstract void hello();
+	@Override
+	abstract void hello();
 }
