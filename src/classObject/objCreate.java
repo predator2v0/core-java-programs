a4239819a66f5d9386046e01965df0d786ae78a0
subhakanta roul <papu.roul99@gmail.com>
2021-06-01 22:19:28 +0530

    source file internal documentation added

--- src/classObject/objCreate.java
+++ src/classObject/objCreate.java
@@ -4,18 +4,20 @@ package classObject;
 // import java.util.*;
 // import java.lang.*;
 public class objCreate {
-    String name;
-    public static void main(String[] args) {
-        try {
-            // BufferedReader b = new BufferedReader(new InputStreamReader(System.in));
-            // Scanner sc = new Scanner(System.in);
-            // b.close();
-            // sc.close();
-            objCreate ob1 = new objCreate();
-            ob1.name = args[0];
-            System.out.println(ob1.name);
-        } catch (Exception e) {
-            e.fillInStackTrace();
-        }
-    }
+	String name;
+
+	public static void main(String[] args) {
+		try {
+			// BufferedReader b = new BufferedReader(new InputStreamReader(System.in));
+			// Scanner sc = new Scanner(System.in);
+			// b.close();
+			// sc.close();
+			objCreate ob1 = new objCreate();
+//          accessing value from command line argument and using it inside program
+			ob1.name = args[0];
+			System.out.println(ob1.name);
+		} catch (Exception e) {
+			e.fillInStackTrace();
+		}
+	}
 }
