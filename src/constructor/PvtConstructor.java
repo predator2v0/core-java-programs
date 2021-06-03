73c353220814e96b5dfb00e2528130f75ae146da
subhakanta roul <papu.roul99@gmail.com>
2021-06-03 16:32:23 +0530

    source file internal documentation added

--- src/constructor/PvtConstructor.java
+++ src/constructor/PvtConstructor.java
@@ -1,8 +1,16 @@
 package constructor;
 
-//class for private constructor dmeo
-class PvtConstructor{
-  static int val = 2890;
-  private PvtConstructor(){
-  }
+
+/*
+ * class for private constructor demo
+ * 
+ * classes with private constructor cannot be instantiated outside the class and hence can not be used to initialize objects outside the
+ * declaring class.
+ * such classes needs to make all their data members as static and are called utility classes.
+ */
+class PvtConstructor {
+	static int val = 2890;
+
+	private PvtConstructor() {
+	}
 }
