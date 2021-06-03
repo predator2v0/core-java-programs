0663645659221a8de5d61a71552fa698bf0f6360
subhakanta roul <papu.roul99@gmail.com>
2021-06-03 16:43:55 +0530

    source file internal documentation added

--- src/constructor/PvtConstructor.java
+++ src/constructor/PvtConstructor.java
@@ -1,9 +1,8 @@
 package constructor;
 
-
 /*
  * class for private constructor demo
- * 
+ 
  * classes with private constructor cannot be instantiated outside the class and hence can not be used to initialize objects outside the
  * declaring class.
  * such classes needs to make all their data members as static and are called utility classes.
