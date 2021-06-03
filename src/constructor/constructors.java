a8979d0a209bd90a5cf329e7ba47ad20f50c2711
subhakanta roul <papu.roul99@gmail.com>
2021-06-03 16:33:03 +0530

    source file internal documentation added

--- src/constructor/constructors.java
+++ src/constructor/constructors.java
@@ -1,25 +1,34 @@
 package constructor;
 
 import methods.ObjMethod;
+
+// driver class for the constructor demo.
 public class constructors {
-    public static void main(String[] args) {
-        constructorDemo cd1 = new constructorDemo();
-        cd1.showData();
-        constructorDemo cd2 = new constructorDemo(2);
-        cd2.showData();
-        constructorDemo cd3 = new constructorDemo("parametrised constructor 2");
-        cd3.showData();
-        constructorDemo cd4 = new constructorDemo(4,"parametrised constructor 3");
-        cd4.showData();
-        constructorChain cc = new constructorChain();
-        System.out.println("cc.x= "+cc.x+"\tcc.y= "+cc.y);
+	public static void main(String[] args) {
+//		calling default constructor.
+		constructorDemo cd1 = new constructorDemo();
+		cd1.showData();
+//		calling parameterized constructors
+		constructorDemo cd2 = new constructorDemo(2);
+		cd2.showData();
+		constructorDemo cd3 = new constructorDemo("parametrised constructor 2");
+		cd3.showData();
+		constructorDemo cd4 = new constructorDemo(4, "parametrised constructor 3");
+		cd4.showData();
+//		demo of constructor chaining.
+		constructorChain cc = new constructorChain();
+		System.out.println("cc.x= " + cc.x + "\tcc.y= " + cc.y);
+		/*
+		 * private constructors can not be used outside the declaring class hence such
+		 * classes can not be instantiated. data members needs to be static for such
+		 * classes and are known as utility classes. 
+		 * PvtConstructor pv = new PvtConstructor(); //can not create object of class with private constructor.
+		 */
+//		accessing static data member of the utility class with a private constructor.
+		System.out.println(PvtConstructor.val);
 
-        System.out.println(PvtConstructor.val);
-        
 //      from ObjMethod.java in package methods;
-        ObjMethod om = new ObjMethod();
-        om.sayHello();
-    }
+		ObjMethod om = new ObjMethod();
+		om.sayHello();
+	}
 }
-
-
