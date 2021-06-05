9827d88aaa2902cc0a80d908dca7caa999b6b641
subhakanta roul <papu.roul99@gmail.com>
2021-06-05 22:31:30 +0530

    exception handling source code refactored and formatted

--- src/exceptionHandling/UserDefinedException.java
+++ src/exceptionHandling/UserDefinedException.java
@@ -3,44 +3,34 @@ package exceptionHandling;
 import java.util.Scanner;
 
 public class UserDefinedException {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.println("enter a number: ");
-        int num = s.nextInt();
-        for (int i = num; i > 0; i--) {
-            try {
-                if (i == 1) {
-                    throw new MyCustomException("cannot divide by 1 :)");
-                }
-                System.out.println(num / i);
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("enter a number: ");
+		int num = s.nextInt();
+		for (int i = num; i > 0; i--) {
+			try {
+				if (i == 1) {
+					throw new MyCustomException("cannot divide by 1 :)");
+				}
+				System.out.println(num / i);
 
-            } catch (MyCustomException me) {
-                System.out.println(me);
-            }
-        }
-        try {
-            myMethod();
-        } catch (MyCustomException m) {
-            System.out.println(m);
-        }
-    }
+			} catch (MyCustomException me) {
+				System.out.println(me);
+			}
+		}
+		try {
+			myMethod();
+		} catch (MyCustomException m) {
+			System.out.println(m);
+		}
+	}
 
-    public static void myMethod() throws MyCustomException {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter a number: ");
-        int num = sc.nextInt();
-        for (int i = num; i > 0; i--) {
-            System.out.println(num / i);
-        }
-    }
-}
-
-class MyCustomException extends Exception {
-    public MyCustomException(String s) {
-        super(s);
-    }
-//    public String toString(){
-//        String exc = " :divided by 1!! not allowed";
-//        return exc;
-//    }
+	public static void myMethod() throws MyCustomException {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter a number: ");
+		int num = sc.nextInt();
+		for (int i = num; i > 0; i--) {
+			System.out.println(num / i);
+		}
+	}
 }
