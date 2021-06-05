8806d7339670ce381ed473fa9ac89e21815d81e1
subhakanta roul <papu.roul99@gmail.com>
2021-06-05 22:29:59 +0530

    exception handling source code refactored and formatted

--- src/exceptionHandling/ChainedException.java
+++ src/exceptionHandling/ChainedException.java
@@ -4,27 +4,27 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class ChainedException {
-    public static void main(String[] args) {
-        ChainedException ce = new ChainedException();
-        System.out.println("enter a and b: ");
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        try {
-            divide(a, b);
-        } catch (Exception ae) {
-            System.out.println(ae.getCause());
-            System.out.println(ae);
-        }
-    }
+	public static void main(String[] args) {
+		ChainedException ce = new ChainedException();
+		System.out.println("enter a and b: ");
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		try {
+			divide(a, b);
+		} catch (Exception ae) {
+			System.out.println(ae.getCause());
+			System.out.println(ae);
+		}
+	}
 
-    public static void divide(int a, int b) {
-        if (b == 0) {
-            ArithmeticException ae = new ArithmeticException();
-            ae.initCause(new IOException("cant take 0 as input in division"));
-            throw ae;
-        } else {
-            System.out.println(a / b);
-        }
-    }
+	public static void divide(int a, int b) {
+		if (b == 0) {
+			ArithmeticException ae = new ArithmeticException();
+			ae.initCause(new IOException("cant take 0 as input in division"));
+			throw ae;
+		} else {
+			System.out.println(a / b);
+		}
+	}
 }
