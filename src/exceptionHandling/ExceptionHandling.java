555828b448a000f21d5e592a2eaadf86fa27d77d
subhakanta roul <papu.roul99@gmail.com>
2021-06-05 22:30:17 +0530

    exception handling source code refactored and formatted

--- src/exceptionHandling/ExceptionHandling.java
+++ src/exceptionHandling/ExceptionHandling.java
@@ -3,90 +3,68 @@ package exceptionHandling;
 import java.util.Scanner;
 
 public class ExceptionHandling extends CustomExceptionHandler {
-    public static void main(String[] args) throws Exception {
-        int a = 0;
+	public static void main(String[] args) throws Exception {
+		int a = 0;
 //        try block with multiple catch blocks
-        try {
-            int b = 7 / a;
-            System.out.println(b);
-        } catch (ArithmeticException e) {
-            if (e instanceof ArithmeticException) {
-                System.out.println("1. arithmetic exception");
-            } else {
-                System.out.println("1. any other exception");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+		try {
+			int b = 7 / a;
+			System.out.println(b);
+		} catch (ArithmeticException e) {
+			if (e instanceof ArithmeticException) {
+				System.out.println("1. arithmetic exception");
+			} else {
+				System.out.println("1. any other exception");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 //        nested try blocks.
-        try {
-            System.out.println("welcome to the exception program");
-            try {
-                int[] ar = new int[2];
-                System.out.println(ar[2]);
-            } catch (ArithmeticException e) {
-                System.out.println("3. ArrayIndexOutOfBound");
-            }
-        } catch (Exception ee) {
-            System.out.println("4. hello exception");
-        }
+		try {
+			System.out.println("welcome to the exception program");
+			try {
+				int[] ar = new int[2];
+				System.out.println(ar[2]);
+			} catch (ArithmeticException e) {
+				System.out.println("3. ArrayIndexOutOfBound");
+			}
+		} catch (Exception ee) {
+			System.out.println("4. hello exception");
+		}
 //        try block with resources
-        // Java 7 onwards supports declaration of resource inside try
-        try (Scanner sc = new Scanner(System.in)) {
-            System.out.println("enter a number");
-            int number = sc.nextInt();
-        } catch (Exception e) {
-            System.out.println("5. Exception:" + e);
-        }
-        // java 9 onwards supports resource declaration outside of the try();
-        Scanner s = new Scanner(System.in);
+		// Java 7 onwards supports declaration of resource inside try
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("enter a number");
+			int number = sc.nextInt();
+			number = number/0;
+		} catch (Exception e) {
+			System.out.println("5. Exception:" + e);
+		}
+		// java 9 onwards supports resource declaration outside of the try();
+		Scanner s = new Scanner(System.in);
 //        try (s) {	/* java 9 onwards*/
-          try {
-            System.out.println("enter a String:");
-            String name = s.nextLine();
-            System.out.println(name);
-        } catch (Exception e) {
-            System.out.println("6. Exception: " + e);
-        }
-        CustomExceptionHandler ceh = new CustomExceptionHandler();
-        try {
-            ceh.getId();
-        } catch (Exception e) {
-            System.out.println("7. Exception: " + e);
-        }
-        try {
-            ceh.getName();
-        } catch (Exception e) {
-            System.out.println("8.2. exception: " + e);
-        } finally {
-            System.out.println("9. finally blcok getting executed.");
-            ExceptionHandling eh = new ExceptionHandling();
-            System.out.println(eh instanceof CustomExceptionHandler);
-            System.out.println(ceh instanceof ExceptionHandling);
-        }
-        s.close();
-    }
-}
-
-//        throw, throws and finally demo.
-class CustomExceptionHandler {
-    int id;
-    String name;
-
-    public void getId() throws ArithmeticException, NoSuchFieldException {
-        System.out.println("enter the id:");
-        Scanner sc = new Scanner(System.in);
-        this.id = sc.nextInt();
-    }
-
-    public void getName() {
-        System.out.println("enter a name: ");
-        Scanner s = new Scanner(System.in);
-        try {
-//            this.name = s.nextLine();
-            throw new Exception("8.no input");
-        } catch (Exception e) {
-            System.out.println("8.Exception: " + e);
-        }
-    }
+//		try {
+//			System.out.println("enter a String:");
+//			String name = s.nextLine();
+//			System.out.println(name);
+//		} catch (Exception e) {
+//			System.out.println("6. Exception: " + e);
+//		}
+		CustomExceptionHandler ceh = new CustomExceptionHandler();
+		try {
+			ceh.getId();
+		} catch (Exception e) {
+			System.out.println("7. Exception: " + e);
+		}
+		try {
+			ceh.getName();
+		} catch (Exception e) {
+			System.out.println("8.2. exception: " + e);
+		} finally {
+			System.out.println("9. finally blcok getting executed.");
+			ExceptionHandling eh = new ExceptionHandling();
+			System.out.println(eh instanceof CustomExceptionHandler);
+			System.out.println(ceh instanceof ExceptionHandling);
+		}
+		s.close();
+	}
 }
