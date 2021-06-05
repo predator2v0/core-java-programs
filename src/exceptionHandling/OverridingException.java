d92b8f10c394125d34605d7ad8cb3baa0cc9d9c5
subhakanta roul <papu.roul99@gmail.com>
2021-06-05 22:31:15 +0530

    exception handling source code refactored and formatted

--- src/exceptionHandling/OverridingException.java
+++ src/exceptionHandling/OverridingException.java
@@ -1,23 +1,17 @@
 package exceptionHandling;
 
 public class OverridingException extends OverriddenException {
-    public static void main(String[] args) {
-        OverridingException oe = new OverridingException();
-        try {
-            oe.hello();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
+	public static void main(String[] args) {
+		OverridingException oe = new OverridingException();
+		try {
+			oe.hello();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 
-    @Override
-    public void hello() throws ArithmeticException{
-        System.out.println("hello from child");
-    }
-}
-
-class OverriddenException {
-    public void hello()  {
-        System.out.println("Hello");
-    }
+	@Override
+	public void hello() throws ArithmeticException {
+		System.out.println("hello from child");
+	}
 }
