a5dc1f4e31d5a4684b00d30148709d98a63d11c4
subhakanta roul <papu.roul99@gmail.com>
2021-06-06 20:42:38 +0530

    source file internal documentation added

--- src/exceptionHandling/UserDefinedException.java
+++ src/exceptionHandling/UserDefinedException.java
@@ -10,21 +10,23 @@ public class UserDefinedException {
 		for (int i = num; i > 0; i--) {
 			try {
 				if (i == 1) {
+//					throwing the custom exception created by the user.
 					throw new MyCustomException("cannot divide by 1 :)");
 				}
 				System.out.println(num / i);
-
+//			handling the custom user defined exception in the catch block.
 			} catch (MyCustomException me) {
 				System.out.println(me);
 			}
 		}
 		try {
 			myMethod();
+//		handling the custom user defined exception in the catch block.
 		} catch (MyCustomException m) {
 			System.out.println(m);
 		}
 	}
-
+//	throwing the custom exception by the method using throws keyword.
 	public static void myMethod() throws MyCustomException {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("enter a number: ");
