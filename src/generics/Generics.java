18c6fe18ed12b26029662d33109f6510fbd965e7
subhakanta roul <papu.roul99@gmail.com>
2021-06-07 22:31:05 +0530

    source file internal documentation added

--- src/generics/Generics.java
+++ src/generics/Generics.java
@@ -1,74 +1,86 @@
 package generics;
 
+/*
+ * in java, generic  are introduced in version 1.5
+ * generic  provide type safety to classes as well as methods.
+ * we can make a class or method generic with a generic data type which later can be changed to 
+ * any supported data type as per requirement.
+ * hence providing the facility of code reusing too.
+ * generic does not support fundamental data types and only works with objects
+ * hence in case fundamental data types required to be used
+ * we can use corresponding wrapper class object.
+ */
 public class Generics {
-    int val;
-    float fl;
+	int val;
+	float fl;
+
 //    generic constructor in a non generic class
-    <V extends Integer, S extends Float> Generics(V v,S s){
-        val = v;
-        fl = s;
-        System.out.println(val +" "+fl);
-    }
-    public static void main(String[] args) {
-//        generic object initialization
-        GenClass<Integer> obj1 = new GenClass<>(5, 6);
-        System.out.println(obj1.getOb() + " " + obj1.getX());
-        GenClass<String> obj2 = new GenClass<>("HELLO WORLD", 6);
-        System.out.println(obj2.getOb() + " " + obj1.getX());
-        GenClass<Character> obj3 = new GenClass<>('c', 6);
-        System.out.println(obj3.getOb() + " " + obj1.getX());
+	<V extends Integer, S extends Float> Generics(V v, S s) {
+		val = v;
+		fl = s;
+		System.out.println(val + " " + fl);
+	}
 
-        GenericClass<Integer,String> obj4 = new GenericClass<>(5,"hello");
-        System.out.println(obj4.getTob());
-        System.out.println(obj4.getUob());
+	public static void main(String[] args) {
+//      generic object initialization
+		GenClass<Integer> obj1 = new GenClass<>(5, 6);
+		System.out.println(obj1.getOb() + " " + obj1.getX());
+		GenClass<String> obj2 = new GenClass<>("HELLO WORLD", 6);
+		System.out.println(obj2.getOb() + " " + obj1.getX());
+		GenClass<Character> obj3 = new GenClass<>('c', 6);
+		System.out.println(obj3.getOb() + " " + obj1.getX());
 
-        System.out.println(add(5,5));
+		GenericClass<Integer, String> obj4 = new GenericClass<>(5, "hello");
+		System.out.println(obj4.getTob());
+		System.out.println(obj4.getUob());
 
-        Generics gs = new Generics(195,220f);
+		System.out.println(add(5, 5));
 
+		Generics gs = new Generics(195, 220f);
 
-    }
+	}
+
 //    generic method in a non generic class
-    public static <T extends Integer> int add(T t, T u){
-        int sum = t.intValue() + u.intValue();
-        return sum;
-    }
+	public static <T extends Integer> int add(T t, T u) {
+		int sum = t.intValue() + u.intValue();
+		return sum;
+	}
 }
 
 // generic class example
 class GenClass<T> {
-    public T ob;
-    private int x;
+	public T ob;
+	private int x;
 
-    GenClass(T ob, int x) {
-        this.ob = ob;
-        this.x = x;
-    }
+	GenClass(T ob, int x) {
+		this.ob = ob;
+		this.x = x;
+	}
 
-    public T getOb() {
-        return ob;
-    }
+	public T getOb() {
+		return ob;
+	}
 
-    public int getX() {
-        return x;
-    }
+	public int getX() {
+		return x;
+	}
 }
 
 // generic class with multiple parameter example
 class GenericClass<T, U> {
-    public T tob;
-    public U uob;
+	public T tob;
+	public U uob;
 
-    public GenericClass(T tob, U uob) {
-        this.tob = tob;
-        this.uob = uob;
-    }
+	public GenericClass(T tob, U uob) {
+		this.tob = tob;
+		this.uob = uob;
+	}
 
-    public T getTob() {
-        return tob;
-    }
+	public T getTob() {
+		return tob;
+	}
 
-    public U getUob() {
-        return uob;
-    }
+	public U getUob() {
+		return uob;
+	}
 }
