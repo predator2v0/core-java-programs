cb6b1ffb0b664ef0b668cb52583ee334bd73a80a
subhakanta roul <papu.roul99@gmail.com>
2021-06-10 19:32:30 +0530

    source code refactored and internal documentation added

--- src/inheritance/inheritance.java
+++ src/inheritance/inheritance.java
@@ -1,12 +1,11 @@
 package inheritance;
 
-import java.util.Scanner;
-
+// driver class for the demonstration of various types of inheritance in java.
 public class inheritance {
 	public static void main(String[] args) {
 		// single inheritance
-		Singleparent sp = new Singleparent();
 		System.out.println("single inheritance: ");
+		Singleparent sp = new Singleparent();
 		sp.add();
 		Singlechild sc = new Singlechild();
 		sc.multiply();
@@ -22,77 +21,14 @@ public class inheritance {
 		mc.display();
 		mc.displaySum();
 		mc.displayTripleSum();
-		// heirarchial inheritance
-		System.out.println("heirarchial inheritance: ");
-		HeirarchialParent hp = new HeirarchialParent();
+		// hierarchial inheritance
+		System.out.println("hierarchial inheritance: ");
+		HierarchialParent hp = new HierarchialParent();
 		hp.sum(5, 7);
 		hp.sub(7, 5);
-		HeirarchialChildOne hco = new HeirarchialChildOne();
+		HierarchialChildOne hco = new HierarchialChildOne();
 		hco.displaySum();
-		HeirarchialChildTwo hct = new HeirarchialChildTwo();
+		HierarchialChildTwo hct = new HierarchialChildTwo();
 		hct.displaySub();
-	}
-}
-
-// start code refactoring from here.
-class Multiintermediate extends Multiparent {
-	int b;
-
-	void displaySum() {
-		Scanner s = new Scanner(System.in);
-		System.out.println("enter child class number: ");
-		b = s.nextInt();
-		System.out.println("enter parent class number: ");
-		a = s.nextInt();
-		System.out.println("child a: " + b + "\t parent a: " + a + "\t sum: " + (b + a));
-	}
-}
-
-class Multichild extends Multiintermediate {
-	int b;
-
-	void displayTripleSum() {
-		Scanner s = new Scanner(System.in);
-		System.out.println("enter child class number: ");
-		b = s.nextInt();
-		System.out.println("enter intermediate class number: ");
-		super.b = s.nextInt();
-		System.out.println("enter the parent class number: ");
-		a = s.nextInt();
-		System.out.println(
-				"child: " + b + "\t intermediate: " + super.b + "\t parent: " + a + "\n sum: " + (a + super.b + b));
-	}
-}
-
-// hirarchial inheritance
-class HeirarchialParent {
-	void sum(int a, int b) {
-		System.out.println("sum: " + (a + b));
-	}
-
-	void sub(int x, int y) {
-		System.out.println("Difference: " + (x - y));
-	}
-}
-
-class HeirarchialChildOne extends HeirarchialParent {
-	void displaySum() {
-		Scanner s = new Scanner(System.in);
-		System.out.println("Enter first number: ");
-		int a = s.nextInt();
-		System.out.println("Enter the second number: ");
-		int b = s.nextInt();
-		sum(a, b);
-	}
-}
-
-class HeirarchialChildTwo extends HeirarchialParent {
-	void displaySub() {
-		Scanner s = new Scanner(System.in);
-		System.out.println("Enter the first number: ");
-		int a = s.nextInt();
-		System.out.println("Enter the second number: ");
-		int b = s.nextInt();
-		sub(a, b);
 	}
 }
