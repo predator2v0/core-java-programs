9fea4330abf64299ca93e1c9ce5ed8c801d74fbe
subhakanta roul <papu.roul99@gmail.com>
2021-06-10 19:34:01 +0530

    source code refactored and internal documentation added

---
+++ src/inheritance/HierarchialParent.java
@@ -0,0 +1,12 @@
+package inheritance;
+
+//demonstration of hierarchial inheritance
+class HierarchialParent {
+	void sum(int a, int b) {
+		System.out.println("sum: " + (a + b));
+	}
+
+	void sub(int x, int y) {
+		System.out.println("Difference: " + (x - y));
+	}
+}
