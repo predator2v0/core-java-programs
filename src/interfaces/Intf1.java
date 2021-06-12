ac15af10599c2d2d277de597de7c68286c5b5656
subhakanta roul <papu.roul99@gmail.com>
2021-06-12 21:54:22 +0530

    source code internal documentation added

--- src/interfaces/Intf1.java
+++ src/interfaces/Intf1.java
@@ -1,7 +1,13 @@
 package interfaces;
 
+/*
+ * by default, all the data members of the interface are public static final, and all the methods are public abstract, even if they are not
+ * declared so.
+ */
 interface Intf1 extends Intf3, Intf4 {
+//	public static data member of the interface
 	int id = 5;
 
+//	public abstract method of the interface
 	void show();
 }
