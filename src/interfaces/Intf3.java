c711d637e79d019b424118bd54c4cbade66ed6b5
subhakanta roul <papu.roul99@gmail.com>
2021-06-12 21:54:33 +0530

    source code internal documentation added

--- src/interfaces/Intf3.java
+++ src/interfaces/Intf3.java
@@ -1,7 +1,13 @@
 package interfaces;
 
+/*
+ * by default, all the data members of the interface are public static final, and all the methods are public abstract, even if they are not
+ * declared so.
+ */
 interface Intf3 {
+//	public static final data members of the interface
 	int id = 8;
 
+//	public abstract method of the interface
 	void show();
 }
