a47fa9fb71d6fdd77385d46dc43429ad375d9341
subhakanta roul <papu.roul99@gmail.com>
2021-06-12 21:54:12 +0530

    source code internal documentation added

--- src/interfaces/Interface.java
+++ src/interfaces/Interface.java
@@ -1,14 +1,19 @@
 package interfaces;
 
 // driver class for interface demonstration
+// class implementing multiple interfaces.
 public class Interface implements Intf2, Intf1 {
 	public static void main(String[] args) {
 		Interface itfs = new Interface();
+//		invoking the overridden method of interfaces in the child class using the object of child class.
 		itfs.show();
+//		invoking the default method(java 8 onwards)
 		itfs.defaultMethod();
+//		invoking the static method using the interface name. (java 8 onwards)
 		Intf4.hello();
 	}
 
+//  Overriding the abstract method of the interfaces in the inheriting concrete class.
 	@Override
 	public void show() {
 		System.out.println(Intf1.id);
