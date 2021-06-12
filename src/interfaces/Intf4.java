ea5c9c9e871ab7aabf4ba5123eef0ff263123b45
subhakanta roul <papu.roul99@gmail.com>
2021-06-12 21:54:40 +0530

    source code internal documentation added

--- src/interfaces/Intf4.java
+++ src/interfaces/Intf4.java
@@ -1,8 +1,14 @@
 package interfaces;
 
+/*
+ * by default, all the data members of the interface are public static final, and all the methods are public abstract, even if they are not
+ * declared so.
+ */
 interface Intf4 {
+//	public static final data members of the interface.
 	int idd = 7;
 
+//	static methods of the interface(java 8 onwards)
 	static void hello() {
 		System.out.println("static method inside interface with body");
 		System.out.println("hello from static method.");
