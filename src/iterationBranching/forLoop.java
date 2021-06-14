a4c7169a9cc61cee2454604a67213715aedbc0ee
subhakanta roul <papu.roul99@gmail.com>
2021-06-14 22:04:39 +0530

    source code reformatted and internal documentation added

--- src/iterationBranching/forLoop.java
+++ src/iterationBranching/forLoop.java
@@ -1,10 +1,12 @@
 package iterationBranching;
 
+// for loop is an entry control loop. the condition must be strictly satisfied to enter into the loop
+// demonstration of for loop in java
 public class forLoop {
-    public static void main(String[] args) {
-        int n = 10;
-        for(int i = 1; i<=10; i++){
-            System.out.print(n + " * " +i+ " = " +n*i+ "\t");
-        }
-    }
+	public static void main(String[] args) {
+		int n = 10;
+		for (int i = 1; i <= 10; i++) {
+			System.out.print(n + " * " + i + " = " + n * i + "\t");
+		}
+	}
 }
