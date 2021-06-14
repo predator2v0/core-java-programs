e363a1617e0dd70d4ad2d2516586513ab548fc7f
subhakanta roul <papu.roul99@gmail.com>
2021-06-14 22:05:01 +0530

    source code reformatted and internal documentation added

--- src/iterationBranching/ifelse.java
+++ src/iterationBranching/ifelse.java
@@ -1,11 +1,12 @@
 package iterationBranching;
 
-public class ifelse{
-    public static void main(String []args){
-        int marks = 8;
-        if(marks > 40)
-            System.out.println("pass");
-        else
-            System.out.println("fail");
-    }
+// demonstration of if else statement in java
+public class ifelse {
+	public static void main(String[] args) {
+		int marks = 8;
+		if (marks > 40)
+			System.out.println("pass");
+		else
+			System.out.println("fail");
+	}
 }
