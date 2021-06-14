bb0c48e85315040e806578936dcc75a93e572074
subhakanta roul <papu.roul99@gmail.com>
2021-06-14 22:04:09 +0530

    source code reformatted and internal documentation added

--- src/iterationBranching/elseIfLadder.java
+++ src/iterationBranching/elseIfLadder.java
@@ -1,19 +1,17 @@
 package iterationBranching;
 
+// demonstration of the else if ladder in java
 public class elseIfLadder {
-    public static void main(String[] args) {
-        int marks = 5;
-        if(marks >= 60 && marks <100){
-            System.out.println("First Division");
-        }
-        else if(marks >= 50 && marks < 60){
-            System.out.println("Second Divison");
-        }
-        else if(marks >= 30 && marks <50){
-            System.out.println("Third Division");
-        }
-        else{
-            System.out.println("Fail");
-        }
-    }
+	public static void main(String[] args) {
+		int marks = 5;
+		if (marks >= 60 && marks < 100) {
+			System.out.println("First Division");
+		} else if (marks >= 50 && marks < 60) {
+			System.out.println("Second Divison");
+		} else if (marks >= 30 && marks < 50) {
+			System.out.println("Third Division");
+		} else {
+			System.out.println("Fail");
+		}
+	}
 }
