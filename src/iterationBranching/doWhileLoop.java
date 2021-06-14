d9b545fbd5a1e2a5b36f4804255caa0c7e3d347c
subhakanta roul <papu.roul99@gmail.com>
2021-06-14 22:03:48 +0530

    source code reformatted and internal documentation added

--- src/iterationBranching/doWhileLoop.java
+++ src/iterationBranching/doWhileLoop.java
@@ -1,11 +1,13 @@
 package iterationBranching;
 
+// the do while loop is a exit control loop which will execute at least once even if the condition given is false.
+// demonstration of do while loop in java
 public class doWhileLoop {
-    public static void main(String[] args) {
-        int n = 10, i = 1;
-        do{
-            System.out.print(n +" * "+ i +" = "+ (n*i) + "\t");
-            ++i;
-        }while(i <= n);
-    }
+	public static void main(String[] args) {
+		int n = 10, i = 1;
+		do {
+			System.out.print(n + " * " + i + " = " + (n * i) + "\t");
+			++i;
+		} while (i <= n);
+	}
 }
