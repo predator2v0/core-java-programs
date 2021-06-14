07491a4d97d41b33f64c8118b40d86483fbdfed6
subhakanta roul <papu.roul99@gmail.com>
2021-06-14 22:06:05 +0530

    source code reformatted and internal documentation added

--- src/iterationBranching/whileLoop.java
+++ src/iterationBranching/whileLoop.java
@@ -1,11 +1,13 @@
 package iterationBranching;
 
+// while loop is an entry control loop, the condition must be strictly satisfied to enter into the loop
+// demonstration of while loop in java
 public class whileLoop {
-    public static void main(String[] args) {
-        int n = 10, i = 1;
-        while (i <= n) {
-            System.out.print(n +" * "+ i +" = "+ (n*i) + "\t");
-            i++;
-        }
-    }
+	public static void main(String[] args) {
+		int n = 10, i = 1;
+		while (i <= n) {
+			System.out.print(n + " * " + i + " = " + (n * i) + "\t");
+			i++;
+		}
+	}
 }
