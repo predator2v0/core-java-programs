ec91a696894b31af771ba07fe2165b2f57c74a8b
subhakanta roul <papu.roul99@gmail.com>
2021-06-14 22:04:27 +0530

    source code reformatted and internal documentation added

--- src/iterationBranching/forEachLoop.java
+++ src/iterationBranching/forEachLoop.java
@@ -1,12 +1,13 @@
 package iterationBranching;
 
+// demonstration of the for each loop in java
 public class forEachLoop {
-    public static void main(String[] args) {
-        int []arr = {1,2,3,4,5,6,7,8,9,0};
-        int index = 0;
-        for (int i : arr) {
-            System.out.println("arr[" + index + "]:"+i);
-            index++;
-        }
-    }
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+		int index = 0;
+		for (int i : arr) {
+			System.out.println("arr[" + index + "]:" + i);
+			index++;
+		}
+	}
 }
