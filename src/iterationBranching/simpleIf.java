be275f575e6811319461a594c30ca9ad6b626102
subhakanta roul <papu.roul99@gmail.com>
2021-06-14 22:05:35 +0530

    source code reformatted and internal documentation added

--- src/iterationBranching/simpleIf.java
+++ src/iterationBranching/simpleIf.java
@@ -1,10 +1,11 @@
 package iterationBranching;
 
+// demonstration of simple if statement in java
 public class simpleIf {
-    public static void main(String[] args) {
-        int mark = 79;
-        if(mark > 60)
-            System.out.println("first division.");
-    }
-    
+	public static void main(String[] args) {
+		int mark = 79;
+		if (mark > 60)
+			System.out.println("first division.");
+	}
+
 }
