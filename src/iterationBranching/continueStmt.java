be83d372ce1dcca4f56c4eb41e921e1d52f83198
subhakanta roul <papu.roul99@gmail.com>
2021-06-14 22:03:24 +0530

    source code reformatted and internal documentation added

--- src/iterationBranching/continueStmt.java
+++ src/iterationBranching/continueStmt.java
@@ -1,25 +1,25 @@
 package iterationBranching;
 
+// demonstration of continue statements in java
 public class continueStmt {
-    public static void main(String[] args) {
-        int n = 4;
-        //continue statement in loop.
-        System.out.println("continue inside simple loop:");
-        for (int i = 0; i<n; i++){
-            if(i > 3 && i < 7)
-                continue;
-            System.out.println(n +" * "+ i +" = "+ n*i + "\t");
-        }
-        //continue statement with label
-        System.out.println("continue with label:");
-        xyz:
-        for (int j = 0;j <6; j++){
-            if(j==3){
-                System.out.println("transfering control to loop beginning:");
-                continue xyz;
-            }
-            System.out.println(n +" * "+ j +" = "+ n*j + "\t");
-        }
-        System.out.println("hello! you are free of loop now.");
-    }
+	public static void main(String[] args) {
+		int n = 4;
+		// continue statement in loop.
+		System.out.println("continue inside simple loop:");
+		for (int i = 0; i < n; i++) {
+			if (i > 3 && i < 7)
+				continue;
+			System.out.println(n + " * " + i + " = " + n * i + "\t");
+		}
+		// continue statement with label
+		System.out.println("continue with label:");
+		xyz: for (int j = 0; j < 6; j++) {
+			if (j == 3) {
+				System.out.println("transfering control to loop beginning:");
+				continue xyz;
+			}
+			System.out.println(n + " * " + j + " = " + n * j + "\t");
+		}
+		System.out.println("hello! you are free of loop now.");
+	}
 }
