deb692e1a5d1b6c64ae0a3a42ef96dc7137f39b0
subhakanta roul <papu.roul99@gmail.com>
2021-06-14 22:05:20 +0530

    source code reformatted and internal documentation added

--- src/iterationBranching/nestedIf.java
+++ src/iterationBranching/nestedIf.java
@@ -1,15 +1,15 @@
 package iterationBranching;
 
+// demonstration of nested if statements in java
 public class nestedIf {
-    public static void main(String[] args) {
-        float perc = 5;
-        if(perc < 100){
-            if(perc > 33){
-                System.out.println("pass");
-            }
-            else{
-                System.out.println("fail");
-            }
-        }
-    }
+	public static void main(String[] args) {
+		float perc = 5;
+		if (perc < 100) {
+			if (perc > 33) {
+				System.out.println("pass");
+			} else {
+				System.out.println("fail");
+			}
+		}
+	}
 }
