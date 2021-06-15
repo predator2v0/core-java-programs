b5be5bb37f04d014ee009de91c5eb47f5092557a
subhakanta roul <papu.roul99@gmail.com>
2021-06-15 22:44:25 +0530

    source code reformatted and internal documentation added

--- src/methods/MethodReturnObj.java
+++ src/methods/MethodReturnObj.java
@@ -2,24 +2,24 @@ package methods;
 
 import java.util.Scanner;
 
+// demonstration of method returning an object
 public class MethodReturnObj {
-    public static void main(String[] args) {
-        String nm;
-        int idd;
-        Scanner s = new Scanner(System.in);
-        System.out.println("enter name: ");
-        nm = s.nextLine();
-        System.out.println("enter id: ");
-        idd = s.nextInt();
-        ObjMethod om = getDetails(nm, idd); // initializing by a function returning object
-        // ObjMethod om = new ObjMethod(nm, idd); // initializing by using constructor.
-        System.out.println(om.name);
-        System.out.println(om.id);
-        s.close();
-    }
+	public static void main(String[] args) {
+		String nm;
+		int idd;
+		Scanner s = new Scanner(System.in);
+		System.out.println("enter name: ");
+		nm = s.nextLine();
+		System.out.println("enter id: ");
+		idd = s.nextInt();
+		ObjMethod om = getDetails(nm, idd); // initializing by a function returning object
+		// ObjMethod om = new ObjMethod(nm, idd); // initializing by using constructor.
+		System.out.println(om.name);
+		System.out.println(om.id);
+		s.close();
+	}
 
-    static ObjMethod getDetails(String str, int i) {
-        return new ObjMethod(str, i); // method returning an object.
-    }
+	static ObjMethod getDetails(String str, int i) {
+		return new ObjMethod(str, i); // method returning an object.
+	}
 }
-
