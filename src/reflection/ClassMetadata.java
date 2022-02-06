5cef393d35e28f15a9c37c7e7b67334f79d0c47f
subhakanta roul <papu.roul99@gmail.com>
2022-02-07 00:02:33 +0530

    demo for getting class metadata using reflection api

--- src/reflection/ClassMetadata.java
+++ src/reflection/ClassMetadata.java
@@ -5,10 +5,10 @@ public class ClassMetadata {
 	public static void main(String[] args) throws ClassNotFoundException {
 		// TODO Auto-generated method stub
 		Class classes = Class.forName("reflection.ReflectionStudent");
-		System.out.println("name: "+classes.getName());
-		System.out.println("type name: "+classes.getTypeName());
-		System.out.println("package name: "+classes.getPackage());
-		System.out.println("number of modifiers: "+classes.getModifiers());
+		System.out.println("name: " + classes.getName());
+		System.out.println("type name: " + classes.getTypeName());
+		System.out.println("package name: " + classes.getPackage());
+		System.out.println("number of modifiers: " + classes.getModifiers());
 	}
 
 }
