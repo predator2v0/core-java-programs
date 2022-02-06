948d24c159fb6e8167ce2c25741019d22cc67bdc
subhakanta roul <papu.roul99@gmail.com>
2022-02-07 00:03:10 +0530

    deserialization of object in java

--- src/serialization/DeSerializeObj.java
+++ src/serialization/DeSerializeObj.java
@@ -4,16 +4,17 @@ import java.io.FileInputStream;
 import java.io.ObjectInputStream;
 
 public class DeSerializeObj {
-    public static void main(String[] args){
-        try{
-            String path = "C:/Users/admin/codePractice/java_repo/core_java_from_scratch_studytonight/serial.txt";
-            FileInputStream fis = new FileInputStream(path);
-            ObjectInputStream ois = new ObjectInputStream(fis);
-            SerialObject sos = (SerialObject) ois.readObject(); // reads the byte stream from file and convert it into object.
-            sos.showInfo();
+	public static void main(String[] args) {
+		try {
+			String path = "C:/Users/admin/codePractice/java_repo/core_java_from_scratch_studytonight/serial.txt";
+			FileInputStream fis = new FileInputStream(path);
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			SerialObject sos = (SerialObject) ois.readObject(); // reads the byte stream from file and convert it into
+																// object.
+			sos.showInfo();
 
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-    }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
